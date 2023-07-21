c7edb6d12472b441ee9e3918b6cd7e5850e86327
Gerson Aranibar <aranibargerson28@gmail.com>
2023-07-21 12:43:13 -0500

    update base url

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-const base_url = 'https://api.mercadopago.com';
+const base_url = 'https://server-mercado-pago.vercel.app';
 
 @Injectable({
   providedIn: 'root',
