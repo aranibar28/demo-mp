83afb1022f4a7e70b8ea215652c04a0746904cf0
Gerson Aranibar <aranibargerson28@gmail.com>
2023-07-24 11:17:20 -0500

    update files

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 const base_url = 'https://server-mercado-pago.vercel.app';
